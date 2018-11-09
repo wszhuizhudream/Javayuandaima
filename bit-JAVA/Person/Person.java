4afce4a9d67cb14f5e59b81212c6af2df855361b
wszhuizhudream <1041646364@qq.com>
2018-11-09 16:48:06 +0800

    bitjava

---
+++ bit-JAVA/Person/Person.java
@@ -0,0 +1,61 @@
+// public class Person{
+	// private String name;
+	// private  int age;
+	// private  String secret;
+    // private String gender;
+
+    // //构造方法重载
+    // //构造方法
+    // //方法名：和类名一样
+    // //返回值：无
+	// //根据实际生活一个人需要有一些必要的属性，所以无参构造不应该有
+// // public Person(){
+	
+// // }
+// public Person(String name,String gender){
+	// this.name=name;
+	// this.gender=gender;
+// }	
+// public Person(String name,String gender,int age){
+	// this(name,gender);//this这个方法的调用必须放在构造方法中的第一条语句
+	// this.age=age;
+// }
+// public Person (String name,String gender,int age,String secret){
+	// this(name,gender,age);
+	// this.secret=secret;
+// }
+
+// //方法 行为
+// String personInfo(){
+	// return "这个人叫"+name+",年龄"+age+"，性别"+gender;
+// }
+// //java 类的属性
+// //getter get***() --->***属性名称
+// //封装
+// String getName(){
+	// return name;
+// }
+// int getAge(){
+	// return age;
+// }
+// String Gender(){
+	// return gender;
+// }
+// String getSecret(){
+	// return secret;
+// }
+// void setName(String name){
+	// this.name=name;
+// }
+// void setAge(int age){
+	// this.age=age;
+// }
+// void setGender(String gender){
+	// this.gender=gender;
+// }
+// void setSecret(String secret){
+	// this.secret=secret;
+// }
+// }
+
+
