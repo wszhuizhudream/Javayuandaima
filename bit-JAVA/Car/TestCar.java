8b83b4d65a3f6ebd2bd905b62751283ffc1c8093
wszhuizhudream <1041646364@qq.com>
2018-11-25 17:55:10 +0800

    java2

--- bit-JAVA/Car/TestCar.java
+++
@@ -1,7 +0,0 @@
-public class TestCar{
-	public static void main(String[] agrs){
-		Car car1=new Car("红色",104789,100000);//此时创建了一个对象
-		car1.khapTronPai();
-		System.out.println(car1.carInfo());
-	}
-}
--- bit-JAVA/IntDemo/IntDemo.java
+++
@@ -1,9 +0,0 @@
-class Test{
-	public int num;
-}
-public class IntDemo{
-	public static void main (String[] args){
-		Test test=new Test();
-		System.out.println(test.num);
-	}
-}
