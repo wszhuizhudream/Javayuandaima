5a1893a3dee06442621c3322d343413c481d5a74
wszhuizhudream <1041646364@qq.com>
2018-12-02 18:07:38 +0800

    java-lesson9string

---
+++ bit-JAVA/Basic/LessonString/LessonString.java
@@ -0,0 +1,11 @@
+public class LessonString{
+	public static void main(String[] args){
+		public void toString(int[] a){
+			for(int i=0;i<a.length;i++){
+			System.out.println(a[i]);
+			}
+		}
+		int[] a=new int[]{1,2,3,4,5};
+		System.out.println(a);
+	}
+}
