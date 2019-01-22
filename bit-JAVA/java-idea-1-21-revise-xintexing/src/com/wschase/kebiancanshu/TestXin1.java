5fd80489b26b87b4742a86161bbfe071bf163b2d
wszhuizhudream <1041646364@qq.com>
2019-01-22 18:08:59 +0800

    java-idea-1-21-xintexing

---
+++ bit-JAVA/java-idea-1-21-revise-xintexing/src/com/wschase/kebiancanshu/TestXin1.java
@@ -0,0 +1,13 @@
+package com.wschase.kebiancanshu;
+
+/**可变参数一
+ * 定义一个方法实现多个参数相加返回一个结果
+ * 我们不知道参数到底有几个，所以我们
+ * Author:WSChase
+ * Created:2019/1/21
+ */
+public class TestXin1 {
+    public static void main(String[] args) {
+
+    }
+}
