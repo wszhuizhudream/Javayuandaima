629e8ec681b07bcd029bc30cdb8c269554d71957
wszhuizhudream <1041646364@qq.com>
2019-01-27 09:48:07 +0800

    java-idea-1-27

---
+++ bit-JAVA/java-idea-1-25-revise-io/src/com/wschase/io1/Testio11.java
@@ -0,0 +1,11 @@
+package com.wschase.io1;
+
+/**I/O——1.1File类使用
+ * java.io.File是一个普通的类，直接产生实例化对象即可。
+ * 如果要实例化对象则需要使用两个构造方法。
+ * Author:WSChase
+ * Created:2019/1/25
+ */
+public class Testio11 {
+
+}
