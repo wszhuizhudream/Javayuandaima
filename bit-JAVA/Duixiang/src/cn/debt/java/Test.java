499b9955e399e3b64a37d04956bb4c826158f0d3
wszhuizhudream <1041646364@qq.com>
2019-03-07 14:42:31 +0800

    2019-3-7

---
+++ bit-JAVA/Duixiang/src/cn/debt/java/Test.java
@@ -0,0 +1,37 @@
+package cn.debt.java;
+
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args){
+//	Student s=new Student();
+////	s.number=510;
+////	s.name="王偲";
+////	s.sex="女";
+////	s.age=20;
+////	s.study();
+//	Student s1=new Student();
+//	s1.number=525;
+//	s1.name="康波涛";
+//	s1.sex="男";
+//	s1.age=22;
+//	s=s1;//将s1的地址赋给s,此时s与s1访问同一个空间，所以输出相同的值
+//	s.study();
+//	s1.study();
+//		System.out.println("请输入n:");
+//		Scanner input=new Scanner(System.in);
+//		int n = input.nextInt();
+//		System.out.println("n="+n);
+		
+//	    System.out.println("请输入一个双精度浮点数");
+//		Scanner num=new Scanner(System.in);
+//		Double number=num.nextDouble();
+//		System.out.println("number="+number);
+		
+			int num1=10;
+			int num2=10;
+			int num=0;
+			num=num1+num2;
+			System.out.println("计算结果为："+num);
+		}
+	}
